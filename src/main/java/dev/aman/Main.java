988a352cc4518c672faf314055e2ccde812e9285
githubgitAman <sharma.aman.a30@gmail.com>
2026-06-24 12:59:56 +0530

    Comments added

--- src/main/java/dev/aman/Main.java
+++ src/main/java/dev/aman/Main.java
@@ -5,6 +5,7 @@ package dev.aman;
 public class Main {
     public static void main(String[] args) {
         FlyingDance ostrich = new Ostrich();
+        //Ostrich don't fly but we are implementing FlyingDance interface
         ostrich.fly();
     }
 }
